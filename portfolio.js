50cad831f02680ea6085ba86de81f2c27f6b1b47
yunyoung1819 <yunyoung1819@hanmail.net>
2019-02-09 03:38:48 +0900

    portfoilo about section add

--- portfolio.js
+++ portfolio.js
@@ -34,23 +34,32 @@ document.getElementById('toggleBtn').addEventListener('click', menuToggle);
 
 /* WELCOME AREA */
 var imageSlideIndex = 1;
+showImageSlides(imageSlideIndex);
 
-//showImageSlides(imageSlideIndex);
+function imageSlideTimer() {
+	plusImageSlides(1);
+}
 
-plusImageSlides(1);
+var imageTimer = setInterval(imageSlideTimer, 3000);
 
 function plusImageSlides(n) {
+	clearInterval(imageTimer);
+	imageTimer = setInterval(imageSlideTimer, 3000);
+
 	showImageSlides(imageSlideIndex += n);
 }
 
 function currentImageSlide(n) {
+	clearInterval(imageTimer);
+	imageTimer = setInterval(imageSlideTimer, 3000);
+	
 	showImageSlides(imageSlideIndex = n);
 }
 
 function showImageSlides(n) {
 	var i;
-	var slides = document.getElmentsByClassName('image-slide');
-	var dots = document.getElmentsByClassName('dot');
+	var slides = document.getElementsByClassName('image-slide');
+	var dots = document.getElementsByClassName('dot');
 
 	if(n > slides.length) { imageSlideIndex = 1 }
 	if(n < 1) { imageSlideIndex = slides.length }
@@ -66,4 +75,12 @@ function showImageSlides(n) {
 	slides[imageSlideIndex-1].style.display = 'block';
 	dots[imageSlideIndex-1].className += ' active';
 }
+
+document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null, -1));
+document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null, 1)); 
+
+document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null, 1));
+document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null, 2));
+document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null, 3));
+document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null, 4 ));
 /* Image Slider End */
